633a0b189fa3a27703b277c27a278465d3e5c45b
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-10-19 19:47:18 +0200

    feat:Add Form and its respective styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,11 +22,11 @@ const App = () => {
                         onChange={toggleDarkMode}
                         name="toggleDark"
                     />
-                    <Grid className={classes.grid} constainer spacing={0} justify="center" alignItems="center" style={{ height: '100vh' }}>
+                    <Grid className={classes.grid} container spacing={0} justify="center" alignItems="center" style={{ height: '100vh' }}>
                         <Grid item xs={12} sm={4}>
                             <Details title="Income"/>
                         </Grid>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={3}>
                             <Main />
                         </Grid>
                         <Grid item xs={12} sm={4}>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from '../styles';
+import Form from '../Form/Form';
 
 const Main = () => {
     const classes = useStyles();
@@ -9,13 +10,20 @@ const Main = () => {
     return (
         <>
             <Card className={classes.root}>
-                <CardHeader title="Expense Tracker" />
+                <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
                 <Divider />
                 <CardContent>
                     <Typography align="center" variant="h5" >Total Balance</Typography>
                     <Typography variant="subtitle1" style={{ lineHeight: '1.5rem', marginTop: '20px' }}>$0.00</Typography>
+                    <Divider />
+                    <Form />
                 </CardContent>
-            </Card> 
+                <CardContent className={classes.cardContent}>
+                    <Grid container spacing={2}>
+                        <h1>List goes here</h1>
+                    </Grid>
+                </CardContent>
+            </Card>
         </>
     )
 }
