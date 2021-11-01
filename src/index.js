3092f34e8d4e57f5077b2d1cebc59156e152e89d
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-01 13:19:18 +0100

    feat: intergrate speechly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 import { Provider } from './context/context';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
+    <SpeechProvider appId="87c874c0-8d6a-41aa-b2f9-c9faedf24fde" language="en-US">
     <Provider>
         <App />
-    </Provider>, document.getElementById('root'));
+    </Provider>
+    </SpeechProvider>, document.getElementById('root'));
 
 
 
